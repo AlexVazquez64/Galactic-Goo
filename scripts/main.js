9b15461a586f683640a23e2ee93d3aa33df8f843
Alex Vazquez <evazquez@ecosat.com.mx>
2024-01-16 12:05:58 -0600

    Add
    Se agregó la estructura inicial del juego.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// main.js
+paper.install(window); // Instala Paper.js en el ámbito global
+window.onload = function () {
+  paper.setup(document.getElementById('myCanvas')); // Inicializa Paper.js con el lienzo
+
+  // Aquí irá la lógica de inicio del juego
+}
